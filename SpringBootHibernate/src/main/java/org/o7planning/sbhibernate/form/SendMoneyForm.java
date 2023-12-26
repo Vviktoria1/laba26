487518d402ff0ebfbc4ba7a7a4b92b9e442407a8
Віка Зварко <114394185+Vviktoria1@users.noreply.github.com>
2023-12-26 22:40:18 +0200

    Add files via upload

---
+++ SpringBootHibernate/src/main/java/org/o7planning/sbhibernate/form/SendMoneyForm.java
@@ -0,0 +1,41 @@
+package org.o7planning.sbhibernate.form;
+
+public class SendMoneyForm {
+    private Long fromAccountId;
+    private Long toAccountId;
+    private Double amount;
+
+    public SendMoneyForm() {
+    }
+    public SendMoneyForm(Long fromAccountId, Long toAccountId, Double amount) {
+        this.fromAccountId = fromAccountId;
+        this.toAccountId = toAccountId;
+        this.amount = amount;
+    }
+
+    public Long getFromAccountId() {
+        return fromAccountId;
+    }
+    public void setFromAccountId(Long fromAccountId) {
+        this.fromAccountId = fromAccountId;
+    }
+    public Long getToAccountId() {
+        return toAccountId;
+    }
+    public void setToAccountId(Long toAccountId) {
+        this.toAccountId = toAccountId;
+    }
+    public Double getAmount() {
+        return amount;
+    }
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+}
+//@Transactional(propagation = Propagation.REQUIRES_NEW,
+//                         rollbackFor = BankTransactionException.class)
+//public void sendMoney(Long fromAccountId, Long toAccountId,
+//                       double amount) throws BankTransactionException {
+//    addAmount(toAccountId, amount);
+//    addAmount(fromAccountId, -amount);
+//}
---
+++ SpringBootHibernate/src/test/java/org/o7planning/sbhibernate/SpringBootHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package org.o7planning.sbhibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHibernateApplicationTests {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHibernateApplicationTests.class, args);
+	}
+
+}
